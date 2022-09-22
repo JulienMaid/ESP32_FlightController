3cf9a0cd12c525cb813b627f67181faf12dc3353
Julien B <jbonne79@yahoo.fr>
2022-09-22 21:30:11 +0200

    Création d'une class d'interface controleurMoteurs

---
+++ controleurMoteurs.cpp
@@ -0,0 +1,20 @@
+/*
+ * controleurMoteurs.cpp
+ *
+ *  Created on: 22 sept. 2022
+ *      Author: julien
+ */
+
+#include "controleurMoteurs.h"
+
+controleurMoteurs::controleurMoteurs()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+controleurMoteurs::~controleurMoteurs()
+{
+  // TODO Auto-generated destructor stub
+}
+
