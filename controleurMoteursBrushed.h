ed84ed9fdc645f7e50ca6654805c389f66d00c0b
Julien B <jbonne79@yahoo.fr>
2022-09-26 21:43:54 +0200

    Création d'une classe controleurMoteurBrushed

---
+++ controleurMoteursBrushed.h
@@ -0,0 +1,39 @@
+/*
+ * controleurMoteursBrushless.h
+ *
+ *  Created on: 22 sept. 2022
+ *      Author: julien
+ */
+
+#ifndef CONTROLEURMOTEURSBRUSHED_H_
+#define CONTROLEURMOTEURSBRUSHED_H_
+
+#include "controleurMoteurs.h"
+
+#define FREQUENCE_PWM       5000
+#define RESOLUTION_PWM      10
+#define CANAL_AVG           0
+#define CANAL_AVD           1
+#define CANAL_ARG           2
+#define CANAL_ARD           3
+
+class controleurMoteursBrushed: public controleurMoteurs
+{
+public:
+  controleurMoteursBrushed();
+  virtual ~controleurMoteursBrushed();
+  controleurMoteursBrushed(const controleurMoteursBrushed &other) = delete;
+  controleurMoteursBrushed(controleurMoteursBrushed &&other) = delete;
+  controleurMoteursBrushed& operator=(const controleurMoteursBrushed &other) = delete;
+  controleurMoteursBrushed& operator=(controleurMoteursBrushed &&other) = delete;
+
+  virtual void ConfigurerMoteurs(uint16_t u16_MoteurAvG, uint16_t u16_MoteurAvD,
+      uint16_t u16_MoteurArG, uint16_t u16_MoteurArD) override;
+
+  virtual void FixerNouvellesConsigne(uint16_t u16_ConsigneMoteurAvG,
+      uint16_t u16_ConsigneMoteurAvD, uint16_t u16_ConsigneMoteurArG,
+      uint16_t u16_ConsigneMoteurArD) override;
+
+};
+
+#endif /* CONTROLEURMOTEURSBRUSHED_H_ */
