cc5fe17e843aed2293de9ab94190079f9d954eb6
Julien B <jbonne79@yahoo.fr>
2022-10-09 18:59:24 +0200

    Suppression mutex car inutiles

--- LecturePulseCmd.cpp
+++ LecturePulseCmd.cpp
@@ -1,5 +1,4 @@
 #include "LecturePulseCmd.h"
-#include <Arduino.h>
 #include "trace_debug.h"
 
 uint32_t tu32_TempsVoies[e_NumeroVoie_t::Nombre_Voies] = { 0 };
@@ -7,22 +6,20 @@ volatile uint32_t tu32_ImpulsionVoies[e_NumeroVoie_t::Nombre_Voies] = { 0 };
 e_EtatsVoie_t te_EtatVoies[e_NumeroVoie_t::Nombre_Voies];
 uint8_t tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Nombre_Voies];
 
-SemaphoreHandle_t l_t_MutexLecturePort[e_NumeroVoie_t::Nombre_Voies];
-
-void InitPortCmd(void)
+void InitPortCmd(uint8_t i_u8_portVoie1, uint8_t i_u8_portVoie2, uint8_t i_u8_portVoie3,
+    uint8_t i_u8_portVoie4)
 {
   for (auto i = 0; i < e_NumeroVoie_t::Nombre_Voies; i++)
   {
     tu32_TempsVoies[i] = 0;
     tu32_ImpulsionVoies[i] = 0;
     te_EtatVoies[i] = e_EtatsVoie_t::Inconnu;
-    l_t_MutexLecturePort[i] = xSemaphoreCreateMutex();
   }
 
-  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie1] = PORT_VOIE1;
-  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie2] = PORT_VOIE2;
-  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie3] = PORT_VOIE3;
-  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie4] = PORT_VOIE4;
+  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie1] = i_u8_portVoie1;
+  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie2] = i_u8_portVoie2;
+  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie3] = i_u8_portVoie3;
+  tu8_CorrespondanceNumeroVoies[e_NumeroVoie_t::Voie4] = i_u8_portVoie4;
 
   pinMode(PORT_VOIE1, INPUT_PULLDOWN);
   pinMode(PORT_VOIE2, INPUT_PULLDOWN);
@@ -35,22 +32,22 @@ void InitPortCmd(void)
 
 }
 
-void ISR_ImpulsionCmdVoie1(void)
+void IRAM_ATTR ISR_ImpulsionCmdVoie1(void)
 {
   GestionPortCmd(e_NumeroVoie_t::Voie1);
 }
 
-void ISR_ImpulsionCmdVoie2(void)
+void IRAM_ATTR ISR_ImpulsionCmdVoie2(void)
 {
   GestionPortCmd(e_NumeroVoie_t::Voie2);
 }
 
-void ISR_ImpulsionCmdVoie3(void)
+void IRAM_ATTR ISR_ImpulsionCmdVoie3(void)
 {
   GestionPortCmd(e_NumeroVoie_t::Voie3);
 }
 
-void ISR_ImpulsionCmdVoie4(void)
+void IRAM_ATTR ISR_ImpulsionCmdVoie4(void)
 {
   GestionPortCmd(e_NumeroVoie_t::Voie4);
 }
@@ -94,38 +91,19 @@ void GestionPortCmd(e_NumeroVoie_t i_e_NumeroVoie)
         u32_ImpulsionVoie = VALEUR_IMPULSION_MAXI;
       }
 
-      if ( xSemaphoreTake( l_t_MutexLecturePort[i_e_NumeroVoie], ( TickType_t ) 1 ) == pdTRUE)
-      {
-        tu32_ImpulsionVoies[i_e_NumeroVoie] = u32_ImpulsionVoie;
-      }
-      else
-      {
-        SEND_VTRACE(ERROR, "Error Prise Mutex");
-      }
-
-      xSemaphoreGive(l_t_MutexLecturePort[i_e_NumeroVoie]);
+      tu32_ImpulsionVoies[i_e_NumeroVoie] = u32_ImpulsionVoie;
     }
   }
 }
 
-uint8_t LectureImpulsionsCmd(uint32_t *o_tu32_TableauLargeurImpulsion[4])
+void LectureImpulsionsCmd(uint32_t *o_tu32_TableauLargeurImpulsion[4])
 {
   uint8_t u8_CR = 0;
 
   for (auto indexBcle = 0; indexBcle < e_NumeroVoie_t::Nombre_Voies; indexBcle++)
   {
-    if ( xSemaphoreTake( l_t_MutexLecturePort[indexBcle], ( TickType_t ) 1 ) == pdTRUE)
-    {
-      *o_tu32_TableauLargeurImpulsion[indexBcle] = tu32_ImpulsionVoies[indexBcle];
-    }
-    else
-    {
-      SEND_VTRACE(ERROR, "Error Prise Mutex");
-    }
-
-    xSemaphoreGive(l_t_MutexLecturePort[indexBcle]);
+    *o_tu32_TableauLargeurImpulsion[indexBcle] = tu32_ImpulsionVoies[indexBcle];
 
   }
 
-  return u8_CR;
 }
--- LecturePulseCmd.h
+++ LecturePulseCmd.h
@@ -9,34 +9,36 @@
 #define LECTUREPULSECMD_H_
 
 #include <stdint.h>
+#include <Arduino.h>
 
 #define PORT_VOIE1  0
 #define PORT_VOIE2  0
 #define PORT_VOIE3  0
 #define PORT_VOIE4  0
 
-#define VALEUR_IMPULSION_MINI 1000
-#define VALEUR_IMPULSION_MAXI 2000
+#define VALEUR_IMPULSION_MINI (uint32_t)1000
+#define VALEUR_IMPULSION_MAXI (uint32_t)2000
 
-enum e_NumeroVoie_t
+enum e_NumeroVoie_t : uint8_t
 {
   Voie1 = 0, Voie2, Voie3, Voie4, Nombre_Voies,
 };
 
-enum class e_EtatsVoie_t
+enum e_EtatsVoie_t : uint8_t
 {
   Inconnu = 0, Haut, Bas,
 };
 
-void ISR_ImpulsionCmdVoie1(void);
-void ISR_ImpulsionCmdVoie2(void);
-void ISR_ImpulsionCmdVoie3(void);
-void ISR_ImpulsionCmdVoie4(void);
+void IRAM_ATTR ISR_ImpulsionCmdVoie1(void);
+void IRAM_ATTR ISR_ImpulsionCmdVoie2(void);
+void IRAM_ATTR ISR_ImpulsionCmdVoie3(void);
+void IRAM_ATTR ISR_ImpulsionCmdVoie4(void);
 
-void InitPortCmd(void);
+void InitPortCmd(uint8_t i_u8_portVoie1 = 0, uint8_t i_u8_portVoie2 = 0, uint8_t i_u8_portVoie3 = 0,
+    uint8_t i_u8_portVoie4 = 0);
 
 void GestionPortCmd(e_NumeroVoie_t i_e_NumeroVoie);
 
-uint8_t LectureImpulsionsCmd(uint32_t *o_tu32_TableauLargeurImpulsion[4]);
+void LectureImpulsionsCmd(uint32_t *o_tu32_TableauLargeurImpulsion[4]);
 
 #endif /* LECTUREPULSECMD_H_ */
