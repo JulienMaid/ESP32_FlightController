a49be08772986f4bf357d086c015da4810179bd9
Julien B <jbonne79@yahoo.fr>
2022-11-07 19:19:59 +0100

    Implémentation Calcul consignes + Renommage Variables internes

--- controleurPID.h
+++ controleurPID.h
@@ -14,6 +14,7 @@
 class ControleurPID
 {
 public:
+  ControleurPID();
   virtual ~ControleurPID();
   ControleurPID(const ControleurPID &other) = delete;
   ControleurPID(ControleurPID &&other) = delete;
@@ -25,15 +26,14 @@ public:
   void NouvellesValeursMouvementsAngulaires(float i_f_MvtYaw, float i_f_MvtPitch,
       float i_f_MvtRoll);
 
-  uint8_t RecupererNouvellesConsignesMoteurs(uint16_t *o_u16_AvG, uint16_t *o_u16_AvD,
-      uint16_t *o_u16_ArG, uint16_t *o_u16_ArD);
+  void NouvellesValeursMesures(float i_f_MesureYaw, float i_f_MesurePitch, float i_f_MesureRoll);
 
-  static ControleurPID* RecupererInstance(void);
+  uint8_t RecupererNouvellesConsignesMoteurs(uint16_t &o_u16_AvG, uint16_t &o_u16_AvD,
+      uint16_t &o_u16_ArG, uint16_t &o_u16_ArD);
+
 protected:
 
 private:
-
-  ControleurPID();
 
   void ActualiserControlleurPID();
 
@@ -44,6 +44,13 @@ private:
   void ResetPID(void);
 
   void CalculerErreurs(void);
+
+  void CalculerConsignesYawRollPitch(void);
+
+  float CalculerConsigneYAW(const uint32_t &i_u32_impulsion_Yaw,
+      const uint32_t &i_u32_impulsion_Throttle);
+
+  float CalculerConsignerRollOuPitch(float i_f_angle, const uint32_t &i_u32_channel_pulse);
 
   template<typename T>
   T minMax(T valeur, T valeurMini, T valeurMaxi);
@@ -63,25 +70,30 @@ private:
   uint32_t *m_tu32_CorrespondanceVoies[e_ListeMouvements_t::NbreMouvements];
 
   // ------------- Global variables used for PID controller --------------------
-  float pid_set_points[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Yaw, Pitch, Roll
+  float m_tf_Consignes_Pid[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Yaw, Pitch, Roll
   // Errors
-  float m_tf_errors[e_ListeMouvements_t::NbreMouvements]; // Measured errors (compared to instructions) : [Yaw, Pitch, Roll]
-  float m_tf_delta_err[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Error deltas in that order   : Yaw, Pitch, Roll
-  float m_tf_error_sum[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Error sums (used for integral component) : [Yaw, Pitch, Roll]
-  float m_tf_previous_error[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Last errors (used for derivative component) : [Yaw, Pitch, Roll]
+  float m_tf_Erreurs[e_ListeMouvements_t::NbreMouvements]; // Measured errors (compared to instructions) : [Yaw, Pitch, Roll]
+  float m_tf_Erreurs_Diff[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Error deltas in that order   : Yaw, Pitch, Roll
+  float m_tf_Erreurs_Integrales[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Error sums (used for integral component) : [Yaw, Pitch, Roll]
+  float m_tf_Erreurs_Precedentes[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 }; // Last errors (used for derivative component) : [Yaw, Pitch, Roll]
   // PID coefficients
   float m_tf_Kp[e_ListeMouvements_t::NbreMouvements] = { 4.0, 1.3, 1.3 }; // P coefficients in that order : Yaw, Pitch, Roll
   float m_tf_Ki[e_ListeMouvements_t::NbreMouvements] = { 0.02, 0.04, 0.04 }; // I coefficients in that order : Yaw, Pitch, Roll
   float m_tf_Kd[e_ListeMouvements_t::NbreMouvements] = { 0, 18, 18 }; // D coefficients in that order : Yaw, Pitch, Roll
   // ---------------------------------------------------------------------------
 
-  float angular_motions[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 };
+  float m_tf_Vitesses_Angulaires[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 };
+
+  float m_tf_Mesures_Angulaires[e_ListeMouvements_t::NbreMouvements] = { 0, 0, 0 };
 
   static constexpr float m_f_ValeurMvtMini = -400.0;
   static constexpr float m_f_ValeurMvtMaxi = 400.0;
 
   static constexpr uint32_t m_u32_impulsionMini = 1100;
+  static constexpr uint32_t m_u32_impulsionMediane = 1500;
   static constexpr uint32_t m_u32_impulsionMaxi = 2000;
+
+  static constexpr uint32_t m_u32_demiTempsMortImpulsion = 8;
 
 };
 
